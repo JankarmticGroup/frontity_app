d38fa52056f912280ee4e241db9c7ca785d71143
Jankarmtic <jankarmticgroup@gmail.com>
2021-07-29 11:36:13 -0400

    reactStrap

--- packages/v-1-0/src/components/index.js
+++ packages/v-1-0/src/components/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "reactstrap";
+
 // import connect to manage state
 //import global styled, and css for styling
 import { connect, Global, css, styled } from "frontity";
@@ -30,31 +33,45 @@ const Root = ({ state }) => {
           html {
             font-family: system-ui, Verdana, Arial, sans-serif;
             color: purple;
+            display: grid;
           }
         `}
       />
-
-      <Header>
-        <StyledLogo>
-          <img
-            src={logo}
-            margin-left="0"
-            height="75px"
-            width="75px"
-            alt="Logo"
-          />
-        </StyledLogo>
-        <img src={cart} height="75px" width="75px" alt="Logo" alt="cart" />
-        <img src={services} height="75px" width="75px" alt="services" />
-        <img src={aboutus} height="75px" width="75px" alt="aboutus" />
-        <main>
-          <Switch>
-            <List when={data.isArchive} />
-            <Post when={data.isPost} />
-            <Page when={data.isPage} />
-          </Switch>
-        </main>
-      </Header>
+      <Container>
+        <Header>
+          <Row xs="3">
+            <Col>
+              <img
+                className="logo"
+                src={logo}
+                height="75px"
+                width="75px"
+                alt="Logo"
+              />
+            </Col>
+            <Col>
+              <main>
+                <Switch>
+                  <List when={data.isArchive} />
+                  <Post when={data.isPost} />
+                  <Page when={data.isPage} />
+                </Switch>
+              </main>
+            </Col>
+            <Col>
+              <img
+                src={cart}
+                height="75px"
+                width="75px"
+                alt="Logo"
+                alt="cart"
+              />
+              <img src={services} height="75px" width="75px" alt="services" />
+              <img src={aboutus} height="75px" width="75px" alt="aboutus" />
+            </Col>
+          </Row>
+        </Header>
+      </Container>
     </>
   );
 };
@@ -62,38 +79,8 @@ const Root = ({ state }) => {
 export default connect(Root);
 const Header = styled.header`
   background-color: Black;
-  //border-width: 0 0 0px 0;
-
-  width: 100%;
-  text-align: center;
-  max-width: 900px;
   padding: 1em;
   h1 {
     color: purple;
   }
-`;
-
-const Main = styled.main`
-  max-width: 800px;
-  padding: 1em;
-  margin: auto;
-
-  img {
-    max-width: 100%;
-  }
-  h2 {
-    margin: 0.5em 0;
-  }
-  p {
-    line-height: 1.25em;
-    margin-bottom: 0.75em;
-  }
-  figcaption {
-    color: #828282;
-    font-size: 0.8em;
-    margin-bottom: 1em;
-  }
-`;
-const StyledLogo = styled.logo`
-  margin-left: 0;!important
 `;
